8d72477f67d7387ebd8ddf0d5a13cbcb9780b26e
Fabian Heinz <derFabi95@gmail.com>
2022-04-11 13:14:13 +0200

    Enhanced logging

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,10 @@ const express = require( 'express' );
 const router = express.Router();
 const authenticateJWT = require( '../controller/verifyJWT' );
 const shoppinglist = require( './shoppinglist' );
+const requestIp = require( 'request-ip' );
 
 router.use( ( req, res, next ) => {
-	//console.log(req);
+	console.log( req.method + " request from IP: " + requestIp.getClientIp( req ) + " to " + req.originalUrl + ". Body data: " + JSON.stringify( req.body ) );
 	res.header( 'Cache-Control', 'public, max-age=0' );
 	res.header( 'Content-Type', 'application/json' );
 	next();
